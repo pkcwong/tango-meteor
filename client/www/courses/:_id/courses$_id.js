947a6fabd14e900c5c1f18d3ecae53075be71e85
Christopher Wong <christopherpk1996@gmail.com>
2017-12-27 21:54:44 +0800

    course details ui

---
+++ client/www/courses/:_id/courses$_id.js
@@ -0,0 +1,34 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+Router.route('/courses/:_id', function() {
+	this.render('courses$_id', {
+		data: {
+			_id: this.params['_id']
+		}
+	});
+});
+
+Template.courses$_id.onCreated(function() {
+	this.words = new ReactiveVar([]);
+});
+
+Template.courses$_id.onRendered(function() {
+	$("#id_courses\\$_id_document").val(this.data['_id']);
+});
+
+Template.courses$_id.events({
+
+	'click #id_courses\\$_id_fetch': () => {
+
+	}
+
+});
+
+Template.courses$_id.helpers({
+
+	fetch: () => {
+		return Template.instance().words.get();
+	}
+
+});
