cb002357f25ffd8a2cf1e02afd1a296609cf7f26
Christopher Wong <christopherpk1996@gmail.com>
2017-12-27 23:56:07 +0800

    added loading

--- client/www/courses/:_id/courses$_id.js
+++ client/www/courses/:_id/courses$_id.js
@@ -13,20 +13,24 @@ Template.courses$_id.onCreated(function() {
 });
 
 Template.courses$_id.onRendered(function() {
-	$("#id_courses\\$_id_document").val(this.data['_id']);
+	$("#id_courses\\$_id_input_document").val(this.data['_id']);
 });
 
 Template.courses$_id.events({
 
-	'click #id_courses\\$_id_fetch': () => {
+	'click #id_courses\\$_id_button_fetch': () => {
+		$('#id_courses\\$_id_segment_raw').addClass('loading');
+		$('#id_courses\\$_id_segment_trim').addClass('loading');
 		Meteor.call('browser', {
 			method: 'get',
 			params: {
-				url: $('#id_courses\\$_id_url').val()
+				url: $('#id_courses\\$_id_input_url').val()
 			}
 		}, (err, res) => {
-			$('#id_courses\\$_id_raw').val(res);
-			$('#id_courses\\$_id_trim').val(jQuery(res).text());
+			$('#id_courses\\$_id_textarea_raw').val(res);
+			$('#id_courses\\$_id_textarea_trim').val(jQuery(res).remove('script').text());
+			$('#id_courses\\$_id_segment_raw').removeClass('loading');
+			$('#id_courses\\$_id_segment_trim').removeClass('loading');
 		});
 	}
 });
