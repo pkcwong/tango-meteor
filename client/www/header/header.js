ea1ac83f20a0ff607033e19d639c5e8263e10c6b
Christopher Wong <christopherpk1996@gmail.com>
2018-01-03 18:26:14 +0800

    user account control

--- client/www/header/header.js
+++ client/www/header/header.js
@@ -8,18 +8,34 @@ Template.header.onRendered(function() {
 	$('#id_modal_login').modal();
 	$("#id_header_dropdown_login").hover(
 		() => {
-			$("#id_header_menu_login").stop().fadeIn(500);
+			if (Meteor.user()) {
+				$("#id_header_menu_login").stop().fadeIn(500);
+			}
 		},
 		() => {
-			$("#id_header_menu_login").delay(100).stop().fadeOut(200);
+			if (Meteor.user()) {
+				$("#id_header_menu_login").delay(100).stop().fadeOut(200);
+			}
 		}
 	);
 });
 
 Template.header.events({
 
+	'click #id_header_item_home': () => {
+		window.location = '/';
+	},
+
+	'click #id_header_item_courses': () => {
+		window.location = '/courses';
+	},
+
 	'click #id_header_dropdown_login': () => {
 		$("#id_header_modal_login").modal('show');
+	},
+
+	'click #id_header_button_login': () => {
+		Meteor.loginWithPassword($("#id_header_input_email").val(), $("#id_header_input_password").val());
 	}
 
 });
