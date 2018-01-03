c4b721afc3c98515cad4d0f65e35ed07a333dfcf
Christopher Wong <christopherpk1996@gmail.com>
2018-01-03 15:11:37 +0800

    login ui

--- client/www/header/header.js
+++ client/www/header/header.js
@@ -5,16 +5,23 @@ Template.header.onCreated(function() {
 });
 
 Template.header.onRendered(function() {
-	$('#id_header_dropdown_login').hover(
+	$('#id_modal_login').modal();
+	$("#id_header_dropdown_login").hover(
 		() => {
-			$('#id_header_menu_login').stop().fadeIn(500);
+			$("#id_header_menu_login").stop().fadeIn(500);
 		},
 		() => {
-			$('#id_header_menu_login').delay(100).stop().fadeOut(200);
+			$("#id_header_menu_login").delay(100).stop().fadeOut(200);
 		}
 	);
 });
 
-Template.header.events({});
+Template.header.events({
+
+	'click #id_header_dropdown_login': () => {
+		$("#id_header_modal_login").modal('show');
+	}
+
+});
 
 Template.header.helpers({});
