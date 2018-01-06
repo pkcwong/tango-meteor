32041bd4ea7ea1bbca094af489c9f5052ccc8743
Christopher Wong <christopherpk1996@gmail.com>
2018-01-06 15:10:06 +0800

    refactor admin

--- server/accounts/admin.js
+++ server/accounts/admin.js
@@ -1,16 +1,20 @@
+import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 
 {
 	let exist = Meteor.users.find({
 		username: 'admin'
 	}).count();
-	if (!exist) {
-		Accounts.createUser({
-			username: 'admin',
-			password: 'root',
-			profile: {
-				privilege: 0
-			}
+	if (exist) {
+		Meteor.users.remove({
+			username: 'admin'
 		});
 	}
+	Accounts.createUser({
+		username: 'admin',
+		password: 'root',
+		profile: {
+			privilege: 0
+		}
+	});
 }
